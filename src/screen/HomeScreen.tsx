e8fae1dfdf48703ae0c4da548be069798e23f133
newt <warthogtouky@gmail.com>
2024-01-31 14:11:02 +0300

    config: add nativewind

---
+++ src/screen/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text className="text-xl text-blue-500 font-bold">HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
